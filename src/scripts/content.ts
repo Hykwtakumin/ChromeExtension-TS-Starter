85fae433e085ca2df84a1f0e9469320e56fcc7bb
hykwtakumin <hykwtakumin@gmail.com>
2018-07-18 06:46:20 +0900

    ディレクトリと.gitignoreを追加

--- src/scripts/content.ts
+++ src/scripts/content.ts
@@ -1,3 +1,12 @@
 import chromep from 'chrome-promise';
+import axios from 'axios';
 
 console.log("This is content scripts");
+
+axios.get("https://www.google.co.jp")
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((e) => {
+        console.log(e);
+    });
